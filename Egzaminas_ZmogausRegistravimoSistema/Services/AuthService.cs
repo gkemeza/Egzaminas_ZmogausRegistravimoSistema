ed4874a1db049c21fac5476b11e3ec0cc3284dc4
Gaudrius <gaudriusk@gmail.com>
2024-12-03 21:25:00 +0200

    add auth service.

--- Egzaminas_ZmogausRegistravimoSistema/Services/AuthService.cs
+++ Egzaminas_ZmogausRegistravimoSistema/Services/AuthService.cs
@@ -1,6 +1,25 @@
-﻿namespace Egzaminas_ZmogausRegistravimoSistema.Services
+﻿using Egzaminas_ZmogausRegistravimoSistema.Services.Interfaces;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Egzaminas_ZmogausRegistravimoSistema.Services
 {
-    public class AuthService
+
+    public class AuthService : IAuthService
     {
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512();
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+
+        public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512(passwordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            return computedHash.SequenceEqual(passwordHash);
+        }
     }
 }
