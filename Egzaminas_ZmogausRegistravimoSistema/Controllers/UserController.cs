e2bf139473d28c98b2e6fa3e2db631fa78150637
Gaudrius <gaudriusk@gmail.com>
2024-12-03 21:47:01 +0200

    update.

--- Egzaminas_ZmogausRegistravimoSistema/Controllers/UserController.cs
+++ Egzaminas_ZmogausRegistravimoSistema/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Egzaminas_ZmogausRegistravimoSistema.Dtos.Requests;
+using Egzaminas_ZmogausRegistravimoSistema.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Egzaminas_ZmogausRegistravimoSistema.Controllers
@@ -7,14 +9,23 @@ namespace Egzaminas_ZmogausRegistravimoSistema.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly ILogger<UserController> _logger;
+        private readonly IAuthService _authService;
+
+        public UserController(ILogger<UserController> logger, IAuthService authService)
+        {
+            _logger = logger;
+            _authService = authService;
+        }
+
         /// <summary>
         /// Create a user account
         /// </summary>
         /// <returns></returns>
         [HttpPost("SignUp")]
-        public IActionResult SignUp()
+        public IActionResult SignUp(UserRequest req)
         {
-
+            _logger.LogInformation($"Creating account for '{req.Username}'");
         }
     }
 }
--- Egzaminas_ZmogausRegistravimoSistema/Dtos/Requests/UserRequest.cs
+++ Egzaminas_ZmogausRegistravimoSistema/Dtos/Requests/UserRequest.cs
@@ -5,11 +5,19 @@ namespace Egzaminas_ZmogausRegistravimoSistema.Dtos.Requests
 {
     public class UserRequest
     {
+        /// <summary>
+        /// Username of the account
+        /// </summary>
         [Required]
         [StringLength(50, MinimumLength = 3)]
         public string? Username { get; set; }
+
+        /// <summary>
+        /// Password of the account
+        /// </summary>
         [PasswordValidator]
         public string? Password { get; set; }
+
         /// <summary>
         /// Role of the account
         /// </summary>
