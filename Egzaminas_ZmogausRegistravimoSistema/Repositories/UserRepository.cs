680b3f45904fa4b37e3c5b1fa47d57692b622bf1
Gaudrius <gaudriusk@gmail.com>
2024-12-03 21:18:37 +0200

    add repo.

--- Egzaminas_ZmogausRegistravimoSistema/Repositories/UserRepository.cs
+++ Egzaminas_ZmogausRegistravimoSistema/Repositories/UserRepository.cs
@@ -1,6 +1,36 @@
-﻿namespace Egzaminas_ZmogausRegistravimoSistema.Repositories
+﻿using Egzaminas_ZmogausRegistravimoSistema.Database;
+using Egzaminas_ZmogausRegistravimoSistema.Entities;
+
+namespace Egzaminas_ZmogausRegistravimoSistema.Repositories
 {
-    public class UserRepository
+
+    public interface IUserRepository
     {
+        Guid CreateUser(UserInfo user);
+    }
+
+    public class UserRepository : IUserRepository
+    {
+        private readonly PersonRegistrationContext _context;
+
+        public UserRepository(PersonRegistrationContext context)
+        {
+            _context = context;
+        }
+
+        public Guid CreateUser(UserInfo user)
+        {
+            ArgumentNullException.ThrowIfNull(user);
+
+            var exists = _context.UserInfos.Any(u => u.Username == user.Username);
+            if (exists)
+                throw new ArgumentException("Username already exists");
+
+            _context.UserInfos.Add(user);
+            _context.SaveChanges();
+            return user.Id;
+        }
+
+
     }
 }
