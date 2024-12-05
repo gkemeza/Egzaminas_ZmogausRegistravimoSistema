9a5ffec8132ad15eb92e66a0a1cc82c05054ac59
Gaudrius <gaudriusk@gmail.com>
2024-12-05 21:57:24 +0200

    simplify table name.

--- Egzaminas_ZmogausRegistravimoSistema/Database/PersonRegistrationContext.cs
+++ Egzaminas_ZmogausRegistravimoSistema/Database/PersonRegistrationContext.cs
@@ -5,7 +5,7 @@ namespace Egzaminas_ZmogausRegistravimoSistema.Database
 {
     public class PersonRegistrationContext : DbContext
     {
-        public DbSet<User> UserInfos { get; set; }
+        public DbSet<User> Users { get; set; }
         public DbSet<PersonInfo> PersonInfos { get; set; }
         public DbSet<Residence> Residences { get; set; }
 
--- Egzaminas_ZmogausRegistravimoSistema/Repositories/UserRepository.cs
+++ Egzaminas_ZmogausRegistravimoSistema/Repositories/UserRepository.cs
@@ -17,11 +17,11 @@ namespace Egzaminas_ZmogausRegistravimoSistema.Repositories
         {
             ArgumentNullException.ThrowIfNull(user);
 
-            var exists = _context.UserInfos.Any(u => u.Username == user.Username);
+            var exists = _context.Users.Any(u => u.Username == user.Username);
             if (exists)
                 throw new ArgumentException("Username already exists");
 
-            _context.UserInfos.Add(user);
+            _context.Users.Add(user);
             _context.SaveChanges();
             return user.Id;
         }
@@ -30,25 +30,25 @@ namespace Egzaminas_ZmogausRegistravimoSistema.Repositories
         {
             ArgumentNullException.ThrowIfNull(username);
 
-            return _context.UserInfos.FirstOrDefault(u => u.Username == username);
+            return _context.Users.FirstOrDefault(u => u.Username == username);
         }
 
         public User? GetUserById(Guid id)
         {
-            return _context.UserInfos.FirstOrDefault(u => u.Id == id);
+            return _context.Users.FirstOrDefault(u => u.Id == id);
         }
 
         public bool UserExists(Guid id)
         {
-            return _context.UserInfos.Any(u => u.Id == id);
+            return _context.Users.Any(u => u.Id == id);
         }
 
         public void DeleteUser(Guid id)
         {
-            var user = _context.UserInfos.Find(id);
+            var user = _context.Users.Find(id);
             if (user != null)
             {
-                _context.UserInfos.Remove(user);
+                _context.Users.Remove(user);
                 _context.SaveChanges();
             }
         }
