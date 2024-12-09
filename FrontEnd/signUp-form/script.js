5117b72d0aa8db46c0b5cda958347358563ba2bf
Gaudrius <gaudriusk@gmail.com>
2024-12-09 19:46:34 +0200

    add auto login on signup.

--- FrontEnd/signUp-form/script.js
+++ FrontEnd/signUp-form/script.js
@@ -25,6 +25,45 @@ const onSignUp = async () => {
     if (!response.ok) {
       throw new Error(`Error status: ${response.status}`);
     }
+
+    logIn(username, password);
+  } catch (error) {
+    console.error("Detailed error:", error);
+  }
+};
+
+const logIn = async (username, password) => {
+  if (!username || !password) {
+    // TODO: display error
+    return;
+  }
+
+  // TODO: add validations
+
+  const data = { username, password };
+
+  try {
+    const url = "https://localhost:7066/api/User/Login";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error status: ${response.status}`);
+    }
+
+    const content = await response.json();
+    const token = content.token;
+    console.log("Login response token: ", token);
+
+    localStorage.setItem("JWT", token);
+
+    window.location.href = "../main-page/main.html"; // go to main page
   } catch (error) {
     console.error("Detailed error:", error);
   }
