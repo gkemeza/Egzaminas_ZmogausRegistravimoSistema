3538814a61d389e0125c0bdb20b8dfa166ad3a40
Gaudrius <gaudriusk@gmail.com>
2024-12-10 21:35:03 +0200

    Add UpdatePassword endpoint.

--- Egzaminas_ZmogausRegistravimoSistema/Controllers/ProfileController.cs
+++ Egzaminas_ZmogausRegistravimoSistema/Controllers/ProfileController.cs
@@ -1,5 +1,7 @@
 ﻿using Egzaminas_ZmogausRegistravimoSistema.Dtos.Requests;
+using Egzaminas_ZmogausRegistravimoSistema.Entities;
 using Egzaminas_ZmogausRegistravimoSistema.Repositories.Interfaces;
+using Egzaminas_ZmogausRegistravimoSistema.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
@@ -14,14 +16,16 @@ namespace Egzaminas_ZmogausRegistravimoSistema.Controllers
     {
         private readonly ILogger<ProfileController> _logger;
         private readonly IUserRepository _userRepository;
+        private readonly IAuthService _authService;
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly Guid _userId;
 
         public ProfileController(ILogger<ProfileController> logger, IUserRepository userRepository,
-            IHttpContextAccessor httpContextAccessor)
+            IAuthService authService, IHttpContextAccessor httpContextAccessor)
         {
             _logger = logger;
             _userRepository = userRepository;
+            _authService = authService;
             _httpContextAccessor = httpContextAccessor;
             _userId = new Guid(_httpContextAccessor!.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier)!);
         }
@@ -30,8 +34,8 @@ namespace Egzaminas_ZmogausRegistravimoSistema.Controllers
         [Produces(MediaTypeNames.Application.Json)]
         [Consumes(MediaTypeNames.Application.Json)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult UpdateUsername([FromBody] UpdateUsernameRequest req)
         {
             _logger.LogInformation($"Updating username for user ID: {_userId}");
@@ -40,13 +44,45 @@ namespace Egzaminas_ZmogausRegistravimoSistema.Controllers
             if (user == null)
             {
                 _logger.LogWarning($"User not found with ID: '{_userId}'");
-                return BadRequest("User not found");
+                return NotFound("User not found");
             }
 
             user.Username = req.NewUsername;
             _userRepository.UpdateUser(user);
 
             _logger.LogInformation($"Successfully updated username for user ID: {_userId}");
+            return NoContent();
+        }
+
+        [HttpPut("UpdatePassword")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult UpdatePassword([FromBody] UpdatePasswordRequest req)
+        {
+            _logger.LogInformation($"Updating password for user ID: {_userId}");
+
+            var user = _userRepository.GetUserById(_userId);
+            if (user == null)
+            {
+                _logger.LogWarning($"User not found with ID: '{_userId}'");
+                return NotFound("User not found");
+            }
+            if (!_authService.VerifyPasswordHash(req.CurrentPassword, user.PasswordHash, user.PasswordSalt))
+            {
+                _logger.LogWarning($"Invalid password for user ID: '{_userId}'");
+                return Unauthorized("Current password is incorrect");
+            }
+
+            _authService.CreatePasswordHash(req.NewPassword, out var newPasswordHash, out var newPasswordSalt);
+            user.PasswordHash = newPasswordHash;
+            user.PasswordSalt = newPasswordSalt;
+
+            _userRepository.UpdateUser(user);
+
+            _logger.LogInformation($"Successfully updated password for user ID: {_userId}");
             return NoContent();
         }
     }
---
+++ Egzaminas_ZmogausRegistravimoSistema/Dtos/Requests/UpdatePasswordRequest.cs
@@ -0,0 +1,11 @@
+﻿using Egzaminas_ZmogausRegistravimoSistema.Validators;
+
+namespace Egzaminas_ZmogausRegistravimoSistema.Dtos.Requests
+{
+    public class UpdatePasswordRequest
+    {
+        public string CurrentPassword { get; set; } = null!;
+        [PasswordValidator]
+        public string NewPassword { get; set; } = null!;
+    }
+}
