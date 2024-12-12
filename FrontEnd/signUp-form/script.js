cf6352bc154082891125ed922b57292733f3e3fe
Gaudrius <gaudriusk@gmail.com>
2024-12-12 21:31:31 +0200

    Add photo in onSignUp().

--- FrontEnd/signUp-form/script.js
+++ FrontEnd/signUp-form/script.js
@@ -6,9 +6,8 @@ const onSignUp = async () => {
   const personalId = document.querySelector("#signup-personalId").value;
   const phoneNumber = document.querySelector("#signup-phoneNumber").value;
   const email = document.querySelector("#signup-email").value;
-  // const photoInput = document.querySelector("#signup-photo");
-  // const photo = photoInput.files[0];
-  // console.log("photoFile: ", photo);
+  const photoInput = document.querySelector("#signup-photo");
+  const photo = photoInput.files[0];
   const city = document.querySelector("#signup-city").value;
   const street = document.querySelector("#signup-street").value;
   const houseNumber = document.querySelector("#signup-houseNumber").value;
@@ -22,7 +21,8 @@ const onSignUp = async () => {
     !personalId ||
     !phoneNumber ||
     !email ||
-    // !photo ||
+    !photoInput ||
+    !photo ||
     !city ||
     !street ||
     !houseNumber ||
@@ -35,56 +35,59 @@ const onSignUp = async () => {
 
   // TODO: add validations
 
-  // Prepare FormData
-  // const formData = new FormData();
-  // formData.append("username", username);
-  // formData.append("password", password);
-  // formData.append("firstName", firstName);
-  // formData.append("lastName", lastName);
-  // formData.append("personalId", personalId);
-  // formData.append("phoneNumber", phoneNumber);
-  // formData.append("email", email);
-  // // formData.append("photo", photo);
-  // formData.append("city", city);
-  // formData.append("street", street);
-  // formData.append("houseNumber", houseNumber);
-  // formData.append("roomNumber", roomNumber);
+  const formData = new FormData();
+  formData.append("username", username);
+  formData.append("password", password);
 
-  // console.log("FormData prepared for submission:");
-  // for (const [key, value] of formData.entries()) {
-  //   console.log(`${key}:`, value);
-  // }
+  formData.append("personInfo.firstName", firstName);
+  formData.append("personInfo.lastName", lastName);
+  formData.append("personInfo.personalId", personalId);
+  formData.append("personInfo.phoneNumber", phoneNumber);
+  formData.append("personInfo.email", email);
+  formData.append("personInfo.photo", photo);
 
-  const data = {
-    username,
-    password,
-    personInfo: {
-      firstName,
-      lastName,
-      personalId,
-      phoneNumber,
-      email,
-      residence: {
-        city,
-        street,
-        houseNumber,
-        roomNumber,
-      },
-    },
-  };
+  formData.append("personInfo.residence.city", city);
+  formData.append("personInfo.residence.street", street);
+  formData.append("personInfo.residence.houseNumber", houseNumber);
+  formData.append("personInfo.residence.roomNumber", roomNumber);
 
-  console.log("Payload sent to server:", JSON.stringify(data, null, 2));
+  console.log("FormData prepared for submission:");
+  for (const [key, value] of formData.entries()) {
+    console.log(`${key}:`, value);
+  }
+
+  // const data = {
+  //   username,
+  //   password,
+  //   personInfo: {
+  //     firstName,
+  //     lastName,
+  //     personalId,
+  //     phoneNumber,
+  //     email,
+  //     residence: {
+  //       city,
+  //       street,
+  //       houseNumber,
+  //       roomNumber,
+  //     },
+  //   },
+  // };
+
+  // console.log("Payload sent to server:", JSON.stringify(data, null, 2));
 
   try {
     const url = "https://localhost:7066/api/User/SignUp";
     const response = await fetch(url, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
+      body: formData,
     });
 
     if (!response.ok) {
-      throw new Error(`Error status: ${response.status}`);
+      const errorDetails = await response.text();
+      throw new Error(
+        `HTTP error! status: ${response.status}, details: ${errorDetails}`
+      );
     }
 
     logIn(username, password);
