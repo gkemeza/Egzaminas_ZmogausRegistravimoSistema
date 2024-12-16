9c26f996d930e54e72b907c0c4afd6d55b416255
Gaudrius <gaudriusk@gmail.com>
2024-12-16 22:52:12 +0200

    Add tests.

--- Egzaminas_ZmogausRegistravimoSistemaTests/Controllers/UserControllerTests.cs
+++ Egzaminas_ZmogausRegistravimoSistemaTests/Controllers/UserControllerTests.cs
@@ -1,5 +1,7 @@
 ﻿using AutoFixture;
 using Egzaminas_ZmogausRegistravimoSistema.Dtos.Requests;
+using Egzaminas_ZmogausRegistravimoSistema.Dtos.Results;
+using Egzaminas_ZmogausRegistravimoSistema.Entities;
 using Egzaminas_ZmogausRegistravimoSistema.Mappers.Interfaces;
 using Egzaminas_ZmogausRegistravimoSistema.Repositories.Interfaces;
 using Egzaminas_ZmogausRegistravimoSistema.Services.Interfaces;
@@ -39,6 +41,59 @@ namespace Egzaminas_ZmogausRegistravimoSistema.Controllers.Tests
         }
 
         [Fact]
+        public void GetUserPerson_ReturnsBadRequest_WhenIdIsEmpty()
+        {
+            // Arrange
+            var emptyId = Guid.Empty;
+
+            // Act
+            var result = _controller.GetUserPerson(emptyId);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Invalid ID.", badRequestResult.Value);
+        }
+
+        [Fact]
+        public void GetUserPerson_ReturnsNotFound_WhenUserDoesNotExist()
+        {
+            // Arrange
+            var userId = _fixture.Create<Guid>();
+            _userRepositoryMock.Setup(repo => repo.GetUserById(userId)).Returns((User?)null);
+
+            // Act
+            var result = _controller.GetUserPerson(userId);
+
+            // Assert
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal("User not found", notFoundResult.Value);
+        }
+
+        [Fact]
+        public void GetUserPerson_ReturnsOk_WhenUserExists()
+        {
+            // Arrange
+            var userId = _fixture.Create<Guid>();
+            var user = GetValidUser();
+            var expectedPersonInfo = _fixture.Create<PersonInfoResult>();
+            var photoBytes = _fixture.Create<byte[]>();
+
+            _userRepositoryMock.Setup(repo => repo.GetUserById(userId)).Returns(user);
+            _userMapperMock.Setup(mapper => mapper.Map(user)).Returns(expectedPersonInfo);
+            _photoServiceMock.Setup(service => service.GetPhotoAsByteArray(user.PersonInfo.PhotoPath)).Returns(photoBytes);
+
+            // Act
+            var result = _controller.GetUserPerson(userId);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedPersonInfo = Assert.IsType<PersonInfoResult>(okResult.Value);
+
+            Assert.Equal(expectedPersonInfo, returnedPersonInfo);
+            Assert.Equal(photoBytes, returnedPersonInfo.PhotoBytes);
+        }
+
+        [Fact]
         public void SignUp_ReturnsBadRequest_WhenNoFileUploaded()
         {
             // Arrange
@@ -59,13 +114,115 @@ namespace Egzaminas_ZmogausRegistravimoSistema.Controllers.Tests
         {
             // Arrange
             var signUpRequest = GetValidSignUpRequest();
-            var expectedUserId = Guid.NewGuid();
 
             // Act
             var result = _controller.SignUp(signUpRequest);
 
             // Assert
-            var createdResult = Assert.IsType<CreatedResult>(result);
+            Assert.IsType<CreatedResult>(result);
+        }
+
+        [Fact]
+        public void Login_ReturnsOk_WhenLoginIsSuccessful()
+        {
+            // Arrange
+            var loginRequest = _fixture.Create<LoginRequest>();
+            var user = GetValidUser();
+
+            _userRepositoryMock.Setup(repo => repo.GetUserByUsername(loginRequest.Username))
+            .Returns(user);
+
+            _authServiceMock.Setup(auth => auth.VerifyPasswordHash(
+                loginRequest.Password, user.PasswordHash, user.PasswordSalt))
+                .Returns(true);
+
+            var expectedToken = "test-jwt-token";
+            _jwtServiceMock.Setup(jwt => jwt.GetJwtToken(user))
+                .Returns(expectedToken);
+
+            // Act
+            var result = _controller.Login(loginRequest);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+
+            var returnValue = okResult.Value;
+            Assert.NotNull(returnValue);
+
+            var tokenProperty = returnValue.GetType().GetProperty("Token");
+            Assert.NotNull(tokenProperty);
+            var actualToken = tokenProperty.GetValue(returnValue);
+
+            Assert.Equal(expectedToken, actualToken);
+        }
+
+        [Fact]
+        public void Login_UserNotFound_ReturnsBadRequest()
+        {
+            // Arrange
+            var loginRequest = _fixture.Create<LoginRequest>();
+
+            _userRepositoryMock.Setup(repo => repo.GetUserByUsername(loginRequest.Username))
+                .Returns((User)null);
+
+            // Act
+            var result = _controller.Login(loginRequest);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("User not found", badRequestResult.Value);
+        }
+
+        [Fact]
+        public void Login_InvalidPassword_ReturnsBadRequest()
+        {
+            // Arrange
+            var loginRequest = _fixture.Create<LoginRequest>();
+            var user = GetValidUser();
+
+            _userRepositoryMock.Setup(repo => repo.GetUserByUsername(loginRequest.Username))
+                .Returns(user);
+
+            _authServiceMock.Setup(auth => auth.VerifyPasswordHash(
+                loginRequest.Password,
+                user.PasswordHash,
+                user.PasswordSalt))
+                .Returns(false);
+
+            // Act
+            var result = _controller.Login(loginRequest);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Invalid username or password", badRequestResult.Value);
+        }
+
+        [Fact]
+        public void Delete_ReturnsNotFound_WhenUserDoesNotExist()
+        {
+            // Arrange
+            var userId = _fixture.Create<Guid>();
+            _userRepositoryMock.Setup(repo => repo.UserExists(userId)).Returns(false);
+
+            // Act
+            var result = _controller.Delete(userId);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void Delete_ReturnsNoContent_WhenUserIsDeleted()
+        {
+            // Arrange
+            var userId = _fixture.Create<Guid>();
+            _userRepositoryMock.Setup(repo => repo.UserExists(userId)).Returns(true);
+
+            // Act
+            var result = _controller.Delete(userId);
+
+            // Assert
+            Assert.IsType<NoContentResult>(result);
         }
 
         private SignUpRequest GetValidSignUpRequest()
@@ -86,6 +243,35 @@ namespace Egzaminas_ZmogausRegistravimoSistema.Controllers.Tests
                     Photo = new FormFile(photoContent, 0, photoContent.Length, "file", "photo.jpg"),
                     Residence = new ResidenceRequest
                     {
+                        City = "Vilnius",
+                        Street = "Gedimino pr.",
+                        HouseNumber = 10,
+                        RoomNumber = 20
+                    }
+                }
+            };
+        }
+
+        private User GetValidUser()
+        {
+            return new User
+            {
+                Id = Guid.NewGuid(),
+                Username = "Jonas",
+                PasswordHash = new byte[] { 1, 2, 3 },
+                PasswordSalt = new byte[] { 1, 2, 3 },
+                PersonInfo = new PersonInfo
+                {
+                    Id = 1,
+                    FirstName = "Jonas",
+                    LastName = "Jonaitis",
+                    PersonalIdNumber = 41102126707,
+                    PhoneNumber = "+37012548730",
+                    Email = "jonas@gmail.com",
+                    PhotoPath = "path/photo.png",
+                    Residence = new Residence
+                    {
+                        Id = 1,
                         City = "Vilnius",
                         Street = "Gedimino pr.",
                         HouseNumber = 10,
