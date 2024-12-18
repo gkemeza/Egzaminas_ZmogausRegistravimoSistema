49369f9f7afc7447b4cc1d08207cbad68e24f6b1
Gaudrius <gaudriusk@gmail.com>
2024-12-18 22:09:45 +0200

    clean up.

--- Egzaminas_ZmogausRegistravimoSistema/Entities/User.cs
+++ Egzaminas_ZmogausRegistravimoSistema/Entities/User.cs
@@ -7,8 +7,6 @@ namespace Egzaminas_ZmogausRegistravimoSistema.Entities
         [Key]
         public Guid Id { get; set; }
         public string Username { get; set; } = string.Empty;
-
-        // acknowledgment to developers and the compiler that the property is non-nullable but defers initialization to a later stage.
         public byte[] PasswordHash { get; set; } = null!;
         public byte[] PasswordSalt { get; set; } = null!;
         public string Role { get; set; } = "User";
