7377c55ff6fd5ea3c949188fde85c332ebe40d80
Gaudrius <gaudriusk@gmail.com>
2024-12-19 20:35:59 +0200

    Add test.

---
+++ Egzaminas_ZmogausRegistravimoSistemaTests/Services/PhotoServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Egzaminas_ZmogausRegistravimoSistema.Entities;
+using Egzaminas_ZmogausRegistravimoSistema.Services;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace Egzaminas_ZmogausRegistravimoSistemaTests.Services
+{
+    public class PhotoServiceTests
+    {
+
+        private readonly PhotoService _photoService;
+
+        public PhotoServiceTests()
+        {
+            _photoService = new PhotoService();
+        }
+
+        [Fact]
+        public void UpdateUserPhoto_UserIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+            User user = null;
+            var mockFile = new Mock<IFormFile>();
+            string uploadDirectory = "uploads/";
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentNullException>(() => _photoService.UpdateUserPhoto(user, mockFile.Object, uploadDirectory));
+            Assert.Equal("user", exception.ParamName);
+        }
+    }
+}
